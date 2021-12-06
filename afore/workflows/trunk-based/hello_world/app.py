7f2855b92e02e719e1dc2d0a2dc9ef2f6a52831c
Carlos Bruno E <cbruno@spsolutions.com.mx>
2021-12-06 15:48:55 -0600

    Se agrega opciopn de Tags y se modifica app

--- afore/workflows/trunk-based/hello_world/app.py
+++ afore/workflows/trunk-based/hello_world/app.py
@@ -36,7 +36,7 @@ def lambda_handler(event, context):
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "message": "hello world from Trunk based. Disparando workflow!.",
+            "message": "hello world from Trunk based. Disparando workflow 2.0.1!",
             # "location": ip.text.replace("\n", "")
         }),
     }
